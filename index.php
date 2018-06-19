44131fa7b31bcf84dda1763020b4dd5642d310ac
YannickPost93 <32703078+YannickPost93@users.noreply.github.com>
2018-06-19 14:13:54 +0200

    push lelijke stijl

--- index.php
+++ index.php
@@ -8,22 +8,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
     <title>Collective Dammen</title>
 </head>
 <body>
-    <form action="?" method="POST">
-        <select name="item1">
-            <option value="a1">A1</option>
-            <option value="a2">A2</option>
-            <option value="a3">A3</option>
-        </select>
-        <select name="item2">
-            <option value="a1">A1</option>
-            <option value="a2">A2</option>
-            <option value="a3">A3</option>
-        </select>
-        <input type="submit" value="Speel zet!">
-    </form>
+
+    
+        <form action="?" method="POST">
+            <div class="custom-select">
+                <label for="item1">Vanaf:</label>
+                <select name="item1">
+                    <option value="a1">A1</option>
+                    <option value="a2">A2</option>
+                    <option value="a3">A3</option>
+                </select>
+                <label for="item2">Vanaf:</label>
+                <select name="item2">
+                    <option value="a1">A1</option>
+                    <option value="a2">A2</option>
+                    <option value="a3">A3</option>
+                </select>
+                <input type="submit" value="Speel zet!">
+            </div>
+        </form>
+   
 
     <?php
         if(!empty($_POST)){
